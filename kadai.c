3b80bbb0982fe6dbce721710fafcc5c702b03d8d
Tonoyama <yudaitonoyama@gmail.com>
2021-11-22 13:42:13 +0900

    first commit

---
+++ kadai.c
@@ -0,0 +1,28 @@
+#include "inc/apelib.h"
+
+int main(int argc, char *argv[])
+{
+	IMG in, out;
+	int i, j, k;
+	
+	ReadImage(&in, argv[1]);
+	
+	out.row = in.row;
+	out.col = in.col;
+	out.bas = in.bas;
+	out.type = in.type;
+	
+	MemImage(&out);
+	
+	for(k = 0; k < out.bas; k++){
+		for(i = 0; i < out.row; i++){
+			for(j = 0; j < out.col; j++){
+				out.data[k][i][j] = in.data[k][i][j];
+			}
+		}
+	}
+	
+	WriteImage(&out, argv[2]);
+	
+	return 0;
+}
